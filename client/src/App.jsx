28e6095799215b7354d0a1f89d3121735598ab92
Pratik09W <walalepratik09@gmail.com>
2025-03-20 23:01:03 +0530

    Setup the frontend with tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import  React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+<h1 className="text-4xl font-bold text-blue-600">Welcome to Finance Tracker!</h1>
+    </>
+  )
+}
+
+export default App
